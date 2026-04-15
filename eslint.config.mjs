b34ae2c9463c756f29abcaf8842ff81552915a6c
Mohammedanwarsha1 <mohammedanwarsha2019@gmail.com>
2026-04-15 21:48:40 +0530

    add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+    {
+        ignores: ["dist/**", "node_modules/**"],
+    },
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        files: ["**/*.ts"],
+        languageOptions: {
+            parserOptions: {
+                project: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            "no-unused-vars": "off",
+            //"no-console": "error",
+            "dot-notation": "error",
+            "@typescript-eslint/no-unused-vars": [
+                "warn",
+                { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+            ],
+        },
+    },
+);
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 function welcome(name: string) {
     console.log("welcome, " + name);
+    const user = {
+        name: "anwar",
+    };
+    const name1 = user.name;
+    return name1;
 }
 
 welcome("anwar1");
