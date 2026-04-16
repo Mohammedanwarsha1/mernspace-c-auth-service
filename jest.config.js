ed7d42d58fa25a2f7d17859bf0b2c7d204ae2576
Mohammedanwarsha1 <mohammedanwarsha2019@gmail.com>
2026-04-16 17:10:41 +0530

    add test1

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,7 @@
 export default {
     testEnvironment: "node",
     extensionsToTreatAsEsm: [".ts"],
-    testMatch: [
-        "**/__tests__/**/*.test.ts",
-        "**/?(*.)+(spec|test).ts",
-    ],
+    testMatch: ["**/__tests__/**/*.test.ts", "**/?(*.)+(spec|test).ts"],
     moduleNameMapper: {
         "^(\\.{1,2}/.*)\\.js$": "$1",
     },
