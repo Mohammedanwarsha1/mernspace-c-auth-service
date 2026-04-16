533ae7b62d8d87c3704f39385e9d66de28673a32
Mohammedanwarsha1 <mohammedanwarsha2019@gmail.com>
2026-04-16 17:20:59 +0530

    add test3

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,12 @@
 export default {
     testEnvironment: "node",
     extensionsToTreatAsEsm: [".ts"],
+    watchPathIgnorePatterns: [
+        "<rootDir>/.git/",
+        "<rootDir>/node_modules/",
+        "<rootDir>/dist/",
+    ],
+    modulePathIgnorePatterns: ["<rootDir>/.git/", "<rootDir>/dist/"],
     testMatch: ["**/__tests__/**/*.test.ts", "**/?(*.)+(spec|test).ts"],
     moduleNameMapper: {
         "^(\\.{1,2}/.*)\\.js$": "$1",
