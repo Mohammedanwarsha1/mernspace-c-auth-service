3cf99c44d3636755b35456e08e27e1979643eee5
Mohammedanwarsha1 <mohammedanwarsha2019@gmail.com>
2026-04-16 17:09:41 +0530

    add test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
     {
-        ignores: ["dist/**", "node_modules/**"],
+        ignores: ["dist/**", "node_modules/**", "jest.config.js"],
     },
     js.configs.recommended,
     ...tseslint.configs.recommended,
